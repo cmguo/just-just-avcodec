780ab300737aa828d02dbeba9f58e7c6d4e0a94f
isxxguo <isxxguo@pptv.com>
2013-06-09 05:51:24 +0000

    /ppbox/branches/1.2.0 /src/ppbox 提交遗留的文件 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6916 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ Debuger.cpp
@@ -0,0 +1,27 @@
+// Debuger.cpp
+
+#include "ppbox/avcodec/Common.h"
+#include "ppbox/avcodec/Debuger.h"
+
+#include <ppbox/common/ClassRegister.h>
+#include "ppbox/avcodec/avc/AvcDebuger.h"
+
+namespace ppbox
+{
+    namespace avcodec
+    {
+
+        Debuger::Debuger()
+        {
+        }
+
+        Debuger::~Debuger()
+        {
+        }
+
+        void Debuger::init()
+        {
+        }
+
+    } // namespace avcodec
+} // namespace ppbox
